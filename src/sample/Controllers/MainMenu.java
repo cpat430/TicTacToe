dedd485e5d73d1d6d6419ecfec3aec1c650b559a
Charles <cpat430@aucklanduni.ac.nz>
2019-11-19 20:39:11 +1300

    Made the main menu of TicTacToe through scenebuilder

---
+++ src/sample/Controllers/MainMenu.java
@@ -0,0 +1,42 @@
+package sample.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class MainMenu {
+
+    @FXML
+    private Button playComputerButton;
+
+    @FXML
+    private Button playPlayerButton;
+
+    @FXML
+    private Button settingsButton;
+
+    @FXML
+    private Button helpButton;
+
+    @FXML
+    void playComputer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playPlayer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toHelp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void toSettings(ActionEvent event) {
+
+    }
+
+}
+
